73d51a2ce3e205e6dc599de3fcb47cb9056de485
youngdong <clark.y.d.son@gmail.com>
2017-04-26 14:56:38 +0900

    Source files only

--- build/mpc_nominal/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/youngdong/ROS/MPC/src/mpc_nominal
-# Build directory: /home/youngdong/ROS/MPC/build/mpc_nominal
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
